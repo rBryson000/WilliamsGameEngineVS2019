c35d2c2a88ff0feccbbdeefaaf5500b6080e8ea7
Bryson Riegel <riegelb@rsd407.org>
2021-04-22 11:54:30 -0700

    Got the ship to move

---
+++ Base project/MyGame/MyGame/Ship.cpp
@@ -0,0 +1,32 @@
+#include "Ship.h"
+
+const float SPEED = 0.3f;
+
+void Ship::update(sf::Time& elapsed) {
+	sf::Vector2f pos = sprite_.getPosition();
+	float x = pos.x;
+	float y = pos.y;
+
+	int msElapsed = elapsed.asMilliseconds();
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))  y -= SPEED * msElapsed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) y += SPEED * msElapsed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) x -= SPEED * msElapsed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) x += SPEED * msElapsed;
+
+	sprite_.setPosition(sf::Vector2f(x, y));
+}
+
+
+
+
+Ship::Ship()
+{
+	sprite_.setTexture(GAME.getTexture("Resources/ship.png"));
+	sprite_.setPosition(sf::Vector2f(100, 100));
+}
+
+void Ship::draw()
+{
+	GAME.getRenderWindow().draw(sprite_);
+}
---
+++ Base project/MyGame/MyGame/Ship.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Engine/GameEngine.h"
+
+class Ship : public GameObject
+{
+public:
+	Ship();
+	void draw();
+	void update(sf::Time& elapsed);
+private:
+	sf::Sprite sprite_;
+};
+
+typedef std::shared_ptr<Ship> ShipPtr;
